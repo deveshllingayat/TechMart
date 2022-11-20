4bbc9f23ab08172bb1f047e242813166c095ab32
DEVESH LINGAYAT <106760807+deveshllingayat@users.noreply.github.com>
2022-11-20 22:46:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,74 +12,88 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS FILE-->
     <link rel="stylesheet" href="style.css">
+  <style>
+ .navbar .navbar-nav .nav-link{
+  color: white;
+ }
 
+ .navbar .navbar-nav .nav-link:hover {
+  color: #202020; 
+ }
+ .nav-link {
+     transition: 0.1s
+ }
+ .navbar form button{
+  color:white;
+}
+.navbar form button:hover{
+  color:#202020;
+}
+  </style>
 </head>
 <body>
     <div class="container-fluid p-0">
-        <!--first child-->
-        <nav class="navbar navbar-expand-lg bg-primary">
-  <div class="container-fluid" style="font-family:'Candara';font-size:20px">
-    <img src="./images/Logo.gif" class="logo">
-    <a class="navbar-brand" href="#" style="font-family:Trebuchet MS; color:white;" ></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <!--first child-->
+  <nav class="navbar navbar-expand-lg bg-primary ">
+    <div class="container-fluid" style="font-family:'Trebuchet MS';font-size:16px">
+      <img src="./images/Logo.gif" class="logo">
+      <a class="navbar-brand" href="#" style="font-family:Trebuchet MS;" ></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"style="color:white;">Home</a>
+          <a class="nav-link active" aria-current="page" href="#" >Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"style="color:white;">Products</a>
+          <a class="nav-link" href="#">Products</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"style="color:white;">Register</a>
+          <a class="nav-link" href="#">Register</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"style="color:white;">Contact</a>
+          <a class="nav-link" href="#">Contact</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"style="color:white;"><i class="fa-solid fa-cart-shopping"><sup>1</sup></i> Cart</a>
+          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"><sup>1</sup></i> Cart</a>
         </li><li class="nav-item">
-          <a class="nav-link" href="#"style="color:white;">Total Price : </a>
+          <a class="nav-link" href="#">Total Price : </a>
         </li>
       </ul>
-      <form class="d-flex" role="search" >
-        <input class="form-control me-2" type="search" placeholder="Search Products" aria-label="Search";  >
-        <button class="btn btn-outline-primary" type="submit"style="color:white;"><i class="fa-solid fa-magnifying-glass"></i></button>
+      <form class="d-flex ms-auto" role="search" >
+        <input class="form-control me-2" type="search" placeholder="Search Products" aria-label="Search"style="width:350px;border-radius:20px;">
+        <button class="btn btn-outline-primary" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
       </form>
+      <ul class = "navbar-nav ms-auto "  style="font-family:'Trebuchet MS';font-size:16px">
+      <li class = "nav-item">
+        <a class = "nav-link" href="#">Welcome Guest</a>
+      </li>
+      <li class = "nav-item">
+        <a class = "nav-link" href="#">Login</a>
+      </li>
+  </ul>
     </div>
   </div>
 </nav>
 
-  <!--Second child -->
-  <nav class = "navbar navbar-expand-lg mb-4" style="background-color:darkblue;">
-    <ul class = "navbar-nav me-auto"  style="font-family:'Candara';font-size:20px">
-      <li class = "nav-item">
-        <a class = "nav-link" href="#"style="color:white;">Welcome Guest</a>
-      </li>
-      <li class = "nav-item">
-        <a class = "nav-link" href="#"style="color:white;">Login</a>
-      </li>
-  </ul>
-</nav>
 
 <!-- Third child-->
-<div style="font-family:'Candara';color:white;">
-  <h2 class = "text-center" >APPLE INSPIRE</h3>
+<div style="font-family:'Trebuchet MS';color:white;">
+  <h2 class = "text-center mt-2" >APPLE INSPIRE</h3>
   <p class="text-center">Think Different.</p>
 </div>
 
   <!-- fourth child-->
-  <div class="row">
-    <div class="col-md-10 mt-2">
+  <div class="row ">
+    <div class="col-md-10 mt-2 ">
       <!--Products-->
-      <div class="row">
+      <div class="row ms-auto ">
       <div class="col-md-4 mb-3"style="width: 18rem;">
-          <div class="card">
-            <img src="./images/iphone14.jpg" class="card-img-top" alt="Iphone 14">
+          <div class="card "style="border-radius:20px;margin:8px">
+            <img src="./images/iphone14.jpg" class="card-img-top" alt="Iphone 14"style="border-radius:20px;">
              <div class="card-body">
-                 <h5 class="card-title">Apple Iphone 14</h5>
+                 <h5 class="card-title">Iphone 14</h5>
                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                  <a href="#" class="btn btn-primary">Add to Cart</a>
                  <a href="#" class="btn btn-secondary">View More</a>
@@ -87,10 +101,10 @@
           </div>
         </div>
         <div class="col-md-4 mb-3"style="width: 18rem;">
-          <div class="card">
-            <img src="./images/iphone14Pro.png" class="card-img-top" alt="...">
+          <div class="card"style="border-radius:20px;margin:8px">
+            <img src="./images/iphone14Pro.png" class="card-img-top" alt="..."style="border-radius:20px;">
               <div class="card-body">
-            <h5 class="card-title">Apple Iphone 14 Pro</h5>
+            <h5 class="card-title">Iphone 14 Pro</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" class="btn btn-primary">Add to Cart</a>
             <a href="#" class="btn btn-secondary">View More</a>
@@ -98,10 +112,10 @@
           </div>
         </div>
         <div class="col-md-4 mb-3"style="width: 18rem;">
-          <div class="card">
-            <img src="./images/iphone14ProMax.png" class="card-img-top" alt="...">
+          <div class="card"style="border-radius:20px;margin:8px">
+            <img src="./images/iphone14ProMax.png" class="card-img-top" alt="..."style="border-radius:20px;">
               <div class="card-body">
-                <h5 class="card-title">Apple Iphone 14 Pro Max</h5>
+                <h5 class="card-title">Iphone 14 Pro Max</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Add to Cart</a>
                 <a href="#" class="btn btn-secondary">View More</a>
@@ -109,10 +123,10 @@
           </div>
         </div>
         <div class="col-md-4 mb-3"style="width: 18rem;">
-          <div class="card">
-              <img src="./images/iphone14Pro.png" class="card-img-top" alt="...">
+          <div class="card"style="border-radius:20px;margin:8px">
+              <img src="./images/iphone14Pro.png" class="card-img-top" alt="..."style="border-radius:20px;">
             <div class="card-body">
-              <h5 class="card-title">Apple Iphone 14 Pro</h5>
+              <h5 class="card-title">Iphone 14 Pro</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Add to Cart</a>
               <a href="#" class="btn btn-secondary">View More</a>
@@ -120,10 +134,10 @@
           </div>
         </div>
         <div class="col-md-4 mb-3"style="width: 18rem;">
-          <div class="card">
-              <img src="./images/iphone14Pro.png" class="card-img-top" alt="...">
+          <div class="card"style="border-radius:20px;margin:8px">
+              <img src="./images/iphone14Pro.png" class="card-img-top" alt="..."style="border-radius:20px;">
             <div class="card-body">
-              <h5 class="card-title">Apple Iphone 14 Pro</h5>
+              <h5 class="card-title">Iphone 14 Pro</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Add to Cart</a>
               <a href="#" class="btn btn-secondary">View More</a>
@@ -131,10 +145,10 @@
           </div>
         </div>
         <div class="col-md-4 mb-3"style="width: 18rem;">
-          <div class="card">
-              <img src="./images/iphone14Pro.png" class="card-img-top" alt="...">
+          <div class="card"style="border-radius:20px;margin:8px">
+              <img src="./images/iphone14Pro.png" class="card-img-top" alt="..."style="border-radius:20px;">
             <div class="card-body">
-              <h5 class="card-title">Apple Iphone 14 Pro</h5>
+              <h5 class="card-title">Iphone 14 Pro</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Add to Cart</a>
               <a href="#" class="btn btn-secondary">View More</a>
@@ -147,45 +161,49 @@
     </div>
     
 <!--side nav-->
-    <div class="col-md-2 bg-light p-0"style="font-family:'Candara';">
+    <div class="col-md-2 bg-light p-0"style="font-family:'Trebuchet MS';">
       <ul class="navbar-nav me-auto text-center">
         <li class="nav-item bg-primary">
           <!-- Brands to be displayed -->
           <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
         </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link text-dark"><h5><b>Apple</h5></a>
+        <li class="nav-item"style="background-color:#E8E8E8">
+          <a href="#" class="nav-link text-dark"><img src="https://sguru.org/wp-content/uploads/2018/02/2000px-Apple_Logo.svg_.png"width="40px"height="40px" alt="Apple" ></a>
         </li> <li class="nav-item">
-          <a href="#" class="nav-link text-dark"><h5><b>Samsung</b></h5></a>
+          <a href="#" class="nav-link text-dark"><img src="https://th.bing.com/th/id/R.10f238d6f72b7c964192fa060f9c120a?rik=ks7Oh5Qx7x0F6w&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fsamsung-hd-png-share-2173.png&ehk=%2b8Cvyqe0%2f8j%2btUvezHRMSaU3lY3GJcG5Ipx4myDw4ys%3d&risl=&pid=ImgRaw&r=0"width="100px" height="40px" alt="Samsung"></a>
         </li> <li class="nav-item ">
-          <a href="#" class="nav-link text-dark"><h5><b>Google</b></h5></a>
+          <a href="#" class="nav-link text-dark"style="background-color:#E8E8E8"><img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-icon-logo-png-transparent-svg-vector-bie-supply-14.png"width="40px" height="40px" alt="Google" ></a>
         </li>
         </li> <li class="nav-item ">
-          <a href="#" class="nav-link text-dark"><h5><b>OnePlus</b></h5></a>
+          <a href="#" class="nav-link text-dark"><img src="https://th.bing.com/th/id/OIP.nsEya4TC3E0rYEBUby5LEwHaHa?pid=ImgDet&rs=1" width="40px" height="40px" alt="OnePlus"></a>
         </li></li> <li class="nav-item ">
-          <a href="#" class="nav-link text-dark"><h5><b>Realme</b></h5></a>
+          <a href="#" class="nav-link text-dark"style="background-color:#E8E8E8"><img src="https://th.bing.com/th/id/OIP.TJhiOHMAM8g7mT-LNQMVuwHaHa?pid=ImgDet&rs=1" width="40px" height="40px" alt="Realme" ></a>
         </li>
       </ul>
       <!--Categories to be displayed-->
       <ul class="navbar-nav me-auto text-center">
         <li class="nav-item bg-primary">
           <!-- Brands to be displayed -->
-          <a href="#" class="nav-link text-light"><h4><b>Categories</b></h4></a>
+          <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link text-dark"><h5><b>Laptops</b></h5></a>
-        </li> <li class="nav-item">
-          <a href="#" class="nav-link text-dark"><h5><b>Mobiles & Tablets</b></h5></a>
-        </li> <li class="nav-item ">
-          <a href="#" class="nav-link text-dark"><h5><b>Audio & Accessories</b></h5></a>
+          <a href="#" class="nav-link text-dark"style="background-color:#E8E8E8"><h5>Laptops</h5></a>
+        </li> 
+        <li class="nav-item">
+          <a href="#" class="nav-link text-dark"><h5>Televisions</h5></a>
+        </li>
+        <li class="nav-item ">
+          <a href="#" class="nav-link text-dark"style="background-color:#E8E8E8"><h5>Mobiles & Tablets</h5></a>
+        </li>
+        <li class="nav-item ">
+          <a href="#" class="nav-link text-dark"><h5>Audio & Accessories</h5></a>
         </li>
       </ul>
     </div>
   </div>
 
-<!-- topmost div-->
 </div>
-    <div class="bg-primary p-3 text-center" style="font-family:'Candara';color:white;font-size:20px"><i class="fa-regular fa-copyright"></i> All Rights Reserved by Devesh</div>
+    <div class="bg-primary p-3 text-center" style="font-family:'Candara';color:white;font-size:20px"><i class="fa-regular fa-copyright"></i> All Rights Reserved by TechMart</div>
 
     <!-- Bootstrap Js link-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
