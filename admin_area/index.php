160de11f7f8538cfdc2459efe08cd25d80c764a8
DEVESH LINGAYAT <106760807+deveshllingayat@users.noreply.github.com>
2022-11-23 22:05:56 +0530

    Update index.php

--- admin_area/index.php
+++ admin_area/index.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin DashBoard</title>
+    <!--font awsome link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!--Bootstrap css link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!-- css file-->
@@ -24,14 +26,17 @@
             color:white;
         }
         .navbar .navbar .navbar-nav .nav-link:hover{
-            color:darkgrey;
+           font-size:18px;
         }
         .button .btn{
+           
+            font-size:24px;
+            font-family:'Trebuchet MS';
             color: black;
-            background-color:	#F0F0F0;
+            background-color:#F0F0F0;
         }
         .button .btn :hover{
-              font-weight: bold;
+            color:blue;
         }
     </style>
 </head>
@@ -62,52 +67,52 @@
                     <p class="text-light text-center p-1"style="font-size:16px;font-family:'Candara';">Admin Name</p>
                 </div>
                 <div class="button text-center " >
-                    <buttton class="btn mx-1" style="font-size:24px;font-family:'Candara';">
+                    <buttton class="btn mx-1"s>
                         <a href="" class="nav-link mx-1">
                             Insert Products
                         </a>
                     </buttton>
-                    <buttton class="btn m-2" style="font-size:24px;font-family:'Candara';">  
+                    <buttton class="btn m-2"s>  
                         <a href="" class="nav-link mx-1">
                             View Products
                         </a>
                     </buttton>
-                    <buttton class="btn m-2" style="font-size:24px;font-family:'Candara';">
-                        <a href="" class="nav-link mx-1">
+                    <buttton class="btn m-2"s>
+                        <a href="index.php?insert_category" class="nav-link mx-1">
                             Insert Categories
                         </a>
                     </buttton>
-                    <buttton class="btn m-2" style="font-size:24px;font-family:'Candara';">
+                    <buttton class="btn m-2"s>
                         <a href="" class="nav-link mx-1">
                             View Categories
                         </a>
                     </buttton>
-                    <buttton class="btn m-2" style="font-size:24px;font-family:'Candara';">
-                        <a href="" class="nav-link mx-1">
+                    <buttton class="btn m-2"s>
+                        <a href="index.php?insert_brand" class="nav-link mx-1">
                             Insert Brands
                         </a>
                     </buttton>
-                    <buttton class="btn m-2" style="font-size:24px;font-family:'Candara';">
+                    <buttton class="btn m-2"s>
                         <a href="" class="nav-link mx-1">
                             View Brands
                         </a>
                     </buttton>
-                    <buttton class="btn m-2" style="font-size:24px;font-family:'Candara';">
+                    <buttton class="btn m-2"s>
                         <a href="" class="nav-link mx-1">
                             All Orders
                         </a>
                     </buttton>
-                    <buttton class="btn m-2" style="font-size:24px;font-family:'Candara';">
+                    <buttton class="btn m-2"s>
                         <a href="" class="nav-link mx-1">
                             All Payments
                         </a>
                     </buttton>
-                    <buttton class="btn m-2" style="font-size:24px;font-family:'Candara';">
+                    <buttton class="btn m-2"s>
                         <a href="" class="nav-link mx-1">
                             List Users
                         </a>
                     </buttton>
-                    <buttton class="btn m-2" style="font-size:24px;font-family:'Candara';">
+                    <buttton class="btn m-2">
                         <a href="" class="nav-link mx-1">
                             Logout
                         </a>
@@ -118,6 +123,17 @@
         </div>
     </div>
 
+    <!--fourth child-->
+    <div class="container my-5">
+        <?php 
+        if(isset($_GET['insert_category']))
+            include('insert_categories.php');
+        
+        if(isset($_GET['insert_brand']))
+            include('insert_brands.php')
+        
+        ?>
+    </div>
 
 
     <div class="p-2 text-center footer" style="background-color:darkblue;font-family:'Candara';color:white;font-size:20px"><i class="fa-regular fa-copyright"></i> All Rights Reserved by TechMart</div>
