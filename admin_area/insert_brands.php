898b7af492a0b6a3f659533cc61ca1968301c4ca
DEVESH LINGAYAT <106760807+deveshllingayat@users.noreply.github.com>
2022-11-24 23:35:37 +0530

    Update insert_brands.php

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -1,33 +1,40 @@
 <?php 
   include("../includes/connect.php");
-  if($_POST['insert_brand']){
+  if(isset($_POST['insert_brand'])){
     $brand_title = $_POST['brand_title'];
+    $brand_url = $_POST['brand_url'];
     //select data from database
-    $select_query = "Select * from `brands` where  brand_title = '$brand_title'";
+    $select_query = "Select * from `brands` where  brand_title = '$brand_title'and brand_url='$brand_url'";
+
     $result_select = mysqli_query($con,$select_query);
+
     $number = mysqli_num_rows($result_select);
     if($number>0){
       echo "<script>alert('The Brand is already present inside the database.')</script>";
     }
     else{
-      $insert_query = "insert into `brands` (brand_title) values ('$brand_title')";
+      $insert_query = "insert into `brands` (brand_title,brand_url) values ('$brand_title','$brand_url')";
       $result = mysqli_query($con,$insert_query);
       if($result){
         echo "<script>alert('The Brand has been inserted successfully!')</script>";
         }
       }
     }
-
-
 ?>
+
+<h2 class="text-center text-light" style="font-family:'Trebuchet MS';">Insert Brands</h2>
 
 <form action="" method= "post" class="mb-2">
 <div class="input-group w-90 mb-2">
   <span class="input-group-text" id="basic-addon1"style="background-color:silver ;"><i class="fa-solid fa-receipt"></i></span>
-  <input type="text" class="form-control" name = "brand_title"placeholder="Insert Brand Name"  required/>
+  <input type="text" class="form-control" name = "brand_title"placeholder="Brand Name"  required/>
+</div>
+<div class="input-group w-90 mb-2">
+<span class="input-group-text" id="basic-addon1"style="background-color:silver ;"><i class="fa-solid fa-receipt"></i></span>
+  <input type="text" class="form-control" name = "brand_url"placeholder="Image URL"  required/>
 </div>
 <div class="input-group w-10 mb-2 m-auto">
-<input type="submit" class="bg-primary text-light p-2 my-3 border-0 "style="border-radius:15px;" name = "insert_brand"value="Insert Brands" required/> 
+<input type="submit" class="bg-primary text-light p-2 my-3 border-0 "style="border-radius:15px;" name = "insert_brand"value="Insert Brand" required/> 
  
 </div>
 </form>
